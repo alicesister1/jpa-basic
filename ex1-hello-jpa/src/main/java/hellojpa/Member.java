7edc18f8025fe172a9f6b044f2d300dafb961e90
ykh <alicesister1@gmail.com>
2021-12-15 23:16:04 +0900

    기본 키 맵핑 - TABLE 실습 +

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -5,16 +5,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.TableGenerator;
 
 @Entity
-@SequenceGenerator(name = "MEMBER_SEQ_GENERATOR",
-    sequenceName = "MEMBER_SEQ", // 맵핑할 데이터베이스 시퀀스 이름
-    initialValue = 1, allocationSize = 1)
+@TableGenerator(
+    name = "MEMBER_SEQ_GENERATOR",
+    table = "MY_SEQUENCES",
+    pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
 public class Member {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+  @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
   private Long id;
 
   @Column(name = "name")
