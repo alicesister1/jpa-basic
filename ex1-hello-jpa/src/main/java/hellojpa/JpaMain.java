6bd0b196921db74075bf7c8646c4affa59d95880
ykh <alicesister1@gmail.com>
2021-12-15 22:41:03 +0900

    기본 키 맵핑 - IDENTITY 실습

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,5 @@
 package hellojpa;
 
-import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -17,33 +16,9 @@ public class JpaMain {
     tx.begin();
 
     try {
-      // 회원 등록
-//      Member member = new Member();
-//      member.setId(2L);
-//      member.setName("HelloB");
-//      em.persist(member);
-
-      // 회원 단 건 조회
-//      Member findMember = em.find(Member.class, 1L);
-//      System.out.println("findMembers.id = " + findMember.getId());
-//      System.out.println("findMembers.name = " + findMember.getName());
-
-      // 회원 삭제
-//      em.remove(findMember);
-
-      // 회원 수정
-//      findMember.setName("HelloJPA");
-
-      // 쿼리 작성
-      List<Member> result = em.createQuery("select m from Member as m", Member.class)
-          .setFirstResult(5)
-          .setMaxResults(8)
-          .getResultList();
-
-      for (Member member : result) {
-        System.out.println("member.name = " + member.getName());
-      }
-
+      Member member = new Member();
+//      member.setUsername("C");
+      em.persist(member);
       tx.commit();
     } catch (Exception e) {
       tx.rollback();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,28 +1,42 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+//@SequenceGenerator(name = "MEMBER_SEQ_GENERATOR",
+//    sequenceName = "MEMBER_SEQ",
+//    initialValue = 1, allocationSize = 1)
 public class Member {
 
   @Id
+//  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private String name;
 
-  public Long getId() {
-    return id;
-  }
+  @Column(name = "name")
+  private String username;
+//
+//  private Integer age;
+//
+//  @Enumerated(EnumType.STRING)
+//  private RoleType roleType;
+//
+//  @Temporal(TemporalType.TIMESTAMP)
+//  private Date createdDate;
+//
+//  @Temporal(TemporalType.TIMESTAMP)
+//  private Date lastModifiedDate;
+//
+//  @Lob
+//  private String description;
+//
+//  @Transient
+//  private Integer temp;
 
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
+  public Member() {
   }
 }
